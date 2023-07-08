761469486e68236b9f20fdd901972861054b0710
a.garkusha <a.garkusha@itcsolutions.ru>
2023-07-08 11:41:59 +0300

    Created BrandLink component

---
+++ js/components/brandlink/BrandLink.js
@@ -0,0 +1,31 @@
+/**
+ * @typedef {Object} Data
+ * @property {string} url
+ */
+
+/**
+ * @function BrandLink
+ * @param {Data} data
+ * @param {string} parrentClassName
+ * @returns {string} HTML or empty
+ */
+
+
+export const Brandink = (data, parrentClassName) => {
+  if (Object.keys(data).length !== 1) return '';
+
+  const { url } = links;
+
+  const currentClassName = parrentClassName
+    ? `${parrentClassName}__link`
+    : `link`;
+
+  return `
+    <a
+      class="${currentClassName}"
+      href="${url}"
+    >
+      ${type === 'google' ? IconGoogle() : IconApple()}
+    </a>
+  `;
+};
