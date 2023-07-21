7f4536c29dd5fc6e878d663cced215f10dc79495
a.garkusha <a.garkusha@itcsolutions.ru>
2023-07-21 19:27:10 +0300

    WIP

--- js/components/sectionWarranty/SectionWarranty.js
+++ js/components/sectionWarranty/SectionWarranty.js
@@ -1,29 +1,26 @@
 import {Title} from '../title/Title.js';
 import {Text} from '../text/Text.js';
 import {Image} from './../image/Image.js';
+
 /**
- * @function SectionWarranty
+ * @function SectionSecondary
  * @param {Data} data
  * @returns {string} HTML or empty
  */
 
-export const SectionWarranty = (data) => {
+export const SectionSecondary = (name, data) => {
   
   const dataValue = Object.values(data);
 
   if (dataValue.length !== 3 && dataValue.length !== 4) return '';
 
-  const {name, title, texts, image} = data;
-
-  const className = name 
-    ? name
-    : 'section-warranty';
+  const { title, texts, image } = data;
 
   return `
-    <section class="${className}"> 
-      ${title ? Title(title, className) : ''}
-      ${texts.length > 0 ? texts.map((text) => Text(text, className)).join('') : ''}
-      ${image ? Image(image, className) : ''}
+    <section class="${name}"> 
+      ${title ? Title(title, name) : ''}
+      ${texts.length > 0 ? texts.map((text) => Text(text, name)).join('') : ''}
+      ${image ? Image(image, name) : ''}
     </section>
   `;
 };
