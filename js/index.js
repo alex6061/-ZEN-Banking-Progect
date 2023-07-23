5ccbd308f2bc4fa08fb252d0b466b849259814f3
a.garkusha <a.garkusha@itcsolutions.ru>
2023-07-23 20:03:57 +0300

    Made changes "spaces"

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-import {data} from './data.js';
-import {SectionPrimary} from './components/widgets/SectionPrimary/SectionPrimary.js';
-import {Navigation} from './components/features/Navigation/Navigation.js';
+import { data} from './data.js';
+import { SectionPrimary } from './components/widgets/SectionPrimary/SectionPrimary.js';
+import { Navigation } from './components/features/Navigation/Navigation.js';
 
 const root = document.querySelector('#root');
 
