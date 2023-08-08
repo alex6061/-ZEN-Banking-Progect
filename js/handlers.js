ef9e15d3887bbb46674d71b4b1e4d0a9744ba941
a.garkusha <a.garkusha@itcsolutions.ru>
2023-08-08 20:16:51 +0300

    Global refactoring

--- js/handlers.js
+++ js/handlers.js
@@ -1,22 +1,26 @@
 import { IconSun } from './components/features/Theme/ui/IconSun.js';
 import { IconMoon } from './components/features/Theme/ui/IconMoon.js';
-
+// ===== hendlers theme
 export const handleThemeClick = (event) => {
   const { currentTarget } = event;
   const { theme } = currentTarget.dataset;
 
   const root = document.querySelector('#root')
   const header = document.querySelector('.header')
-  
+  const lang = document.querySelector('#lang') 
+
   if (theme === 'light') {
     currentTarget.dataset.theme = 'dark'
     currentTarget.innerHTML = IconSun();
     // background
     root.classList.add('dark')
     root.classList.remove('light')
-    //header
+    // header
     header.classList.add('dark')
     header.classList.remove('light')
+    // lang
+    lang.classList.add('dark')
+    lang.classList.remove('light')
   };
       
   if (theme === 'dark'){
@@ -25,9 +29,12 @@ export const handleThemeClick = (event) => {
     // background
     root.classList.add('light')
     root.classList.remove('dark')
-    //header
+    // header
     header.classList.add('light')
     header.classList.remove('dark')
+    // lang
+    lang.classList.add('light')
+    lang.classList.remove('dark')
   };
 };
 
