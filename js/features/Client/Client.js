77056051ef43ca1a171a511bb44550329a42c3ae
a.garkusha <a.garkusha@itcsolutions.ru>
2023-08-13 23:35:41 +0300

    Fixed

--- js/features/Client/Client.js
+++ js/features/Client/Client.js
@@ -1,17 +1,3 @@
-import { data } from './../../data.js';
-
-/** @typedef {import('./types.ts').Props}Props */
-
-/**
- * @function Client
- * @param {data} data
- * @param {Object} brand
- * @param {string} parrencClassName
- * @param {Props} source
- * @param {Props} description
- * @returns {HTML}
- */
-
 export const Client = (brand, parrentClassName) => {
   const currentClassName = parrentClassName
     ? `${parrentClassName}__image`
