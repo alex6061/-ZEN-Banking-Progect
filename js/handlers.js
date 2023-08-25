8efdd95eb7e45b73dda9118ef71a0513e92aaa26
a.garkusha <a.garkusha@itcsolutions.ru>
2023-08-25 17:25:38 +0300

    Added a drop-down menu

--- js/handlers.js
+++ js/handlers.js
@@ -4,37 +4,44 @@ export const handleThemeClick = (event) => {
   const { currentTarget } = event;
   const { theme } = currentTarget.dataset;
 
-  const root = document.querySelector('#root')
-  const header = document.querySelector('#header')
-  const lang = document.querySelector('#lang') 
+  const root = document.querySelector('#root');
+  const header = document.querySelector('#header');
+  const lang = document.querySelector('#lang');
+  const $menu = document.querySelector('#menu');
 
   if (theme === 'light') {
-    currentTarget.dataset.theme = 'dark'
+    currentTarget.dataset.theme = 'dark';
     currentTarget.innerHTML = IconSun();
 
-    root.classList.add('dark')
-    root.classList.remove('light')
+    root.classList.add('dark');
+    root.classList.remove('light');
 
-    header.classList.add('dark')
-    header.classList.remove('light')
+    header.classList.add('dark');
+    header.classList.remove('light');
 
-    lang.classList.add('dark')
-    lang.classList.remove('light')
-  };
-      
-  if (theme === 'dark'){
-    currentTarget.dataset.theme = 'light'
+    lang.classList.add('dark');
+    lang.classList.remove('light');
+
+    $menu.classList.add('dark');
+    $menu.classList.remove('light');
+  }
+
+  if (theme === 'dark') {
+    currentTarget.dataset.theme = 'light';
     currentTarget.innerHTML = IconMoon();
 
-    root.classList.add('light')
-    root.classList.remove('dark')
+    root.classList.add('light');
+    root.classList.remove('dark');
 
-    header.classList.add('light')
-    header.classList.remove('dark')
+    header.classList.add('light');
+    header.classList.remove('dark');
 
-    lang.classList.add('light')
-    lang.classList.remove('dark')
-  };
+    lang.classList.add('light');
+    lang.classList.remove('dark');
+
+    $menu.classList.add('light');
+    $menu.classList.remove('dark');
+  }
 };
 
 export const handleBurgerClick = (event) => {
