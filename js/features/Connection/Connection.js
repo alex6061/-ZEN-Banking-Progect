e67cdf87fe89769bde81d7e96096ff933127fd65
a.garkusha <a.garkusha@itcsolutions.ru>
2023-08-26 08:06:10 +0300

    WIP

---
+++ js/features/Connection/Connection.js
@@ -0,0 +1,12 @@
+export const Select = () => {
+
+  const {} = data; 
+
+  return `
+    <select class="select input" placeholder="Connection">
+      <option value="${value}">${content}</option>
+      <option value="${value}">${content}</option>
+      <option value="${value}">${content}</option>
+    </select>
+  `;
+};
